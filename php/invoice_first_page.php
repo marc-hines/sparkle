26d4fa40b5085abc7047e2b904675d4d8de9776e
Marc Hines <marc.hines@adp.com>
2014-01-31 14:08:18 -0800

    corrected name of base tcpdf library

--- php/invoice_first_page.php
+++ php/invoice_first_page.php
@@ -25,7 +25,8 @@
  */
 
 // Include the main TCPDF library (search for installation path).
-require_once('tcpdf_min/tcpdf_include.php');
+require_once('tcpdf_min/tcpdf.php');
+
 
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
